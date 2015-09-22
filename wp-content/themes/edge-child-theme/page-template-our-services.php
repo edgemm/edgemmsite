d0db360b9f5d9d87505a1e1c87030b4243955429
edgemm-gavin <gavin@edgemm.com>
2015-09-22 16:08:32 -0700

    Removing unwanted services

--- wp-content/themes/edge-child-theme/page-template-our-services.php
+++ wp-content/themes/edge-child-theme/page-template-our-services.php
@@ -1,10 +1,9 @@
 <?php /* Template Name: Our Services */ ?>
 <?php get_header(); ?>
-
-<?php if(!is_front_page()):?>
+<?php if(!is_front_page()): ?>
 <div id="pageHead">
-	<h1><?php the_title(); ?></h1>
-	<?php $page_description=g et_post_meta($post->ID, "_ttrust_page_description_value", true); ?>
+	<h1><?php  the_title(); ?></h1>
+	<?php $page_description=get_post_meta($post->ID, "_ttrust_page_description_value", true); ?>
 	<?php if ($page_description) : ?>
 	<p>
 		<?php echo $page_description; ?>
@@ -17,7 +16,6 @@
 		<?php while (have_posts()) : the_post(); ?>
 		<div <?php post_class( 'clearfix'); ?>>
 			<?php the_content(); ?>
-
 			<ul id="filterNav" class="clearfix">
 				<li><a href="#" id="online-servicesbutton">Digital</a>
 				</li>
@@ -25,23 +23,18 @@
 				</li>
 				<li><a href="#" id="creative-servicesbutton">Creative</a>
 				</li>
-				<!--<li><a href="#" id="designbutton">Design</a></li>-->
-				<!-- <li><a href="#" id="public-relationsbutton">Public Relations</a></li> -->
-				<!-- <li><a href="#" id="social-mediabutton">Social Media</a></li> -->
-
 			</ul>
-
 			<!-- Online Services -->
 			<div id="online-services">
 				<div id="posts" class="full homeSection clearfix">
 					<h3><span>Digital</span></h3>
-					<?php query_posts( array( 'order'=> 'ASC','cat' => 154));?>
+					<?php query_posts( array( 'order'=> 'ASC','cat' => 154)); ?>
 					<div class="homePosts">
 						<?php while (have_posts()) : the_post(); ?>
 						<div <?php post_class( 'small'); ?>>
-							<a class="thumb" href="<?php the_permalink() ?>" rel="bookmark">
+							<a class="thumb" href="<?php  the_permalink()  ?>" rel="bookmark">
 								<?php the_post_thumbnail( "ttrust_one_fourth_short", array( 'class'=> 'thumb', 'alt' => ''.get_the_title().'', 'title' => ''.get_the_title().'')); ?></a>
-							<h1><a href="<?php the_permalink() ?>" rel="bookmark" ><?php the_title(); ?></a></h1>
+							<h1><a href="<?php  the_permalink()  ?>" rel="bookmark" ><?php  the_title(); ?></a></h1>
 							<div class="meta clearfix"></div>
 							<?php print_excerpt_smc(get_the_title()); ?>
 							<?php //the_excerpt(); ?>
@@ -53,18 +46,17 @@
 				</div>
 			</div>
 			<!-- End -->
-
 			<!-- Media buying -->
 			<div id="media-buying">
 				<div id="posts" class="full homeSection clearfix">
 					<h3><span>Media Buying</span></h3>
-					<?php query_posts( array( 'order'=> 'ASC','cat' => 170));?>
+					<?php query_posts( array( 'order'=> 'ASC','cat' => 170)); ?>
 					<div class="homePosts">
 						<?php while (have_posts()) : the_post(); ?>
 						<div <?php post_class( 'small'); ?>>
-							<a class="thumb" href="<?php the_permalink() ?>" rel="bookmark">
+							<a class="thumb" href="<?php  the_permalink()  ?>" rel="bookmark">
 								<?php the_post_thumbnail( "ttrust_one_fourth_short", array( 'class'=> 'thumb', 'alt' => ''.get_the_title().'', 'title' => ''.get_the_title().'')); ?></a>
-							<h1><a href="<?php the_permalink() ?>" rel="bookmark" ><?php the_title(); ?></a></h1>
+							<h1><a href="<?php  the_permalink()  ?>" rel="bookmark" ><?php  the_title(); ?></a></h1>
 							<div class="meta clearfix"></div>
 							<?php print_excerpt_smc(get_the_title()); ?>
 							<?php //the_excerpt(); ?>
@@ -76,18 +68,17 @@
 				</div>
 			</div>
 			<!-- End -->
-
 			<!-- Creative Services -->
 			<div id="creative-services">
 				<div id="posts" class="full homeSection clearfix">
 					<h3><span>Creative</span></h3>
-					<?php query_posts( array( 'order'=> 'ASC','cat' => 152));?>
+					<?php query_posts( array( 'order'=> 'ASC','cat' => 152)); ?>
 					<div class="homePosts">
 						<?php while (have_posts()) : the_post(); ?>
 						<div <?php post_class( 'small'); ?>>
-							<a class="thumb" href="<?php the_permalink() ?>" rel="bookmark">
+							<a class="thumb" href="<?php  the_permalink()  ?>" rel="bookmark">
 								<?php the_post_thumbnail( "ttrust_one_fourth_short", array( 'class'=> 'thumb', 'alt' => ''.get_the_title().'', 'title' => ''.get_the_title().'')); ?></a>
-							<h1><a href="<?php the_permalink() ?>" rel="bookmark" ><?php the_title(); ?></a></h1>
+							<h1><a href="<?php  the_permalink()  ?>" rel="bookmark" ><?php  the_title(); ?></a></h1>
 							<div class="meta clearfix"></div>
 							<?php print_excerpt_smc(get_the_title()); ?>
 							<?php //the_excerpt(); ?>
@@ -99,94 +90,18 @@
 				</div>
 			</div>
 			<!-- End -->
-
-			<!-- Design -->
-			<div id="design">
-				<div id="posts" class="full homeSection clearfix">
-					<h3><span>Design</span></h3>
-					<?php query_posts( array( 'order'=> 'ASC','cat' => 23));?>
-					<div class="homePosts">
-						<?php while (have_posts()) : the_post(); ?>
-						<div <?php post_class( 'small'); ?>>
-							<a class="thumb" href="<?php the_permalink() ?>" rel="bookmark">
-								<?php the_post_thumbnail( "ttrust_one_fourth_short", array( 'class'=> 'thumb', 'alt' => ''.get_the_title().'', 'title' => ''.get_the_title().'')); ?></a>
-							<h1><a href="<?php the_permalink() ?>" rel="bookmark" ><?php the_title(); ?></a></h1>
-							<div class="meta clearfix"></div>
-							<?php print_excerpt_smc(get_the_title()); ?>
-							<?php //the_excerpt(); ?>
-							<?php more_link(); ?>
-						</div>
-						<?php endwhile; ?>
-						<?php wp_reset_query(); ?>
-					</div>
-				</div>
-			</div>
-			<!-- End -->
-
-
-
-			<!-- Social Media -->
-			<div id="social-media">
-				<div id="posts" class="full homeSection clearfix">
-					<h3><span>Social Media</span></h3>
-					<?php query_posts( array( 'order'=> 'ASC','cat' => 171));?>
-					<div class="homePosts">
-						<?php while (have_posts()) : the_post(); ?>
-						<div <?php post_class( 'small'); ?>>
-							<a class="thumb" href="<?php the_permalink() ?>" rel="bookmark">
-								<?php the_post_thumbnail( "ttrust_one_fourth_short", array( 'class'=> 'thumb', 'alt' => ''.get_the_title().'', 'title' => ''.get_the_title().'')); ?></a>
-							<h1><a href="<?php the_permalink() ?>" rel="bookmark" ><?php the_title(); ?></a></h1>
-							<div class="meta clearfix"></div>
-							<?php print_excerpt_smc(get_the_title()); ?>
-							<?php //the_excerpt(); ?>
-							<?php more_link(); ?>
-						</div>
-						<?php endwhile; ?>
-						<?php wp_reset_query(); ?>
-					</div>
-				</div>
-			</div>
-			<!-- End -->
-
-			<!-- Public Relations -->
-
-			<div id="public-relations">
-				<div id="posts" class="full homeSection clearfix">
-					<h3><span>Public Relations</span></h3>
-					<?php query_posts( array( 'order'=> 'ASC','cat' => 173));?>
-					<div class="homePosts">
-						<?php while (have_posts()) : the_post(); ?>
-						<div <?php post_class( 'small'); ?>>
-							<a class="thumb" href="<?php the_permalink() ?>" rel="bookmark">
-								<?php the_post_thumbnail( "ttrust_one_fourth_short", array( 'class'=> 'thumb', 'alt' => ''.get_the_title().'', 'title' => ''.get_the_title().'')); ?></a>
-							<h1><a href="<?php the_permalink() ?>" rel="bookmark" ><?php the_title(); ?></a></h1>
-							<div class="meta clearfix"></div>
-							<?php print_excerpt_smc(get_the_title()); ?>
-							<?php //the_excerpt(); ?>
-							<?php more_link(); ?>
-						</div>
-						<?php endwhile; ?>
-						<?php wp_reset_query(); ?>
-					</div>
-				</div>
-			</div>
-			<!-- End -->
-
 		</div>
-		<?php comments_template( '', true); ?>
-		<?php endwhile; ?>
 	</div>
+	<?php comments_template( '', true); ?>
+	<?php endwhile; ?>
 </div>
-
 <script type="text/javascript">
 	jQuery(document).ready(function($) {
-
 		function goToByScroll(id) {
 			$('html,body').animate({
 				scrollTop: $(id).offset().top
 			}, 'slow');
 		}
-
 		$('#media-buyingbutton').click(function() {
 			goToByScroll('#media-buying');
 			theend();
@@ -211,9 +126,6 @@
 			goToByScroll('#public-relations');
 			theend();
 		});
-
 	});
 </script>
-
-
 <?php get_footer(); ?>
